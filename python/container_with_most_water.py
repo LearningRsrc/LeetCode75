0e9c1d52a638a2a053595e683b32b7a73c7f30cd
Muhammed <lawalmuhammedope@gmail.com>
2026-01-06 18:54:16 +0100

    initial commit

---
+++ python/container_with_most_water.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+        def maxArea(self, height: List[int]) -> int:
+                n = len(height)
+                if n<2: return 2
+                if n==2: return min(height[0], height[1])
+                answer_bucket = []
+                right_pointer = n - 1
+                left_pointer = 0
+                while(n >= 2):
+                        area = min(height[right_pointer], height[left_pointer])
+                        answer_bucket.append(area)
+                        if height[right_pointer] < height[left_pointer]:
+                                right_pointer -= 1
+                        else:
+                                left_pointer += 1
+                        n -= 1
+                return max(answer_bucket)
---
+++ python/main.py
@@ -0,0 +1,6 @@
+from container_with_most_water import Solution
+
+if __name__ == "__main__":
+	print("hello world")
+	solution = Solution().maxArea([1,8,6,2,5,4,8,3,7])
+	print(f"Maximum area is: {solution}")
