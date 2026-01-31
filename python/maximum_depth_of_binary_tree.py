28de823d3749093370439036c96f36a60c5dd6e2
Muhammed <lawalmuhammedope@gmail.com>
2026-01-31 23:18:04 +0100

    feat(leetcode): add maximum depth of binary tree

---
+++ python/maximum_depth_of_binary_tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return self.dfs(root)
+        
+    def dfs(self, root):
+        if root == None:
+            return 0
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        ans = 1 + max(left, right)
+        return ans
